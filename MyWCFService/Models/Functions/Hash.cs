262ae9dc351a01300bc8066353956ab7062e35bb
Khai Vu <kenn.mta@gmail.com>
2019-11-20 17:50:31 +0700

    - Create My WCF Service.

---
+++ MyWCFService/Models/Functions/Hash.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyWCFService.Models.Functions
+{
+    public class Hash
+    {
+        public static string CreateToken()
+        {
+            Guid g = Guid.NewGuid();
+            string token = Convert.ToBase64String(g.ToByteArray());
+            token = token.Replace("=", "");
+            token = token.Replace("+", "");
+
+            return token;
+        }
+    }
+}
